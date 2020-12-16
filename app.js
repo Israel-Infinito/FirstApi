5193abcf76a62913fe7e22d4bd68e2321d3e3800
israelalagbe <israelalagbe214@gmail.com>
2020-12-16 12:48:09 +0100

    Made a post request for creating new user in postman

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv/config')
+
+
+//This is a middleware that help parse our requests from web 
+app.use(bodyParser.json());
+
+
+//This connect the post route to the main app
+const postRoute = require('./routes/postroute');
+app.use('/posts', postRoute);
+
+app.use(bodyParser.json());
 
 //Connection to the database on mongodb
 mongoose.connect(process.env.DB_CONNECTION,
@@ -13,6 +25,8 @@ mongoose.connect(process.env.DB_CONNECTION,
 () => console.log ('We are connected to the database')
 );
 
+
+
 app.listen (
-    3000, () => console.log('Server has started')
+    8000, () => console.log('Server has started')
 )
--- routes/postroute.js
+++ routes/postroute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../model/Post');
 
 //This is a post route to get post from the user
 router.get('/', async (req, res) =>{
@@ -13,8 +14,8 @@ router.get('/', async (req, res) =>{
 
 //This helps to post data into the database on the post route
 
-router.poster ('/', async (req, res) =>{
-     const post = new post({
+router.post('/', async (req, res) =>{
+     const post = new Post({
          userName: req.body.userName,
          title: req.body.title
      });
@@ -24,5 +25,9 @@ router.poster ('/', async (req, res) =>{
         } catch (err) {
             res.json({ message: err});
         }
+        
 });
 
+
+
+module.exports = router;
